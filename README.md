3786af0023d411860ad8e6d5b893c39418934f85
Iris Mireya <irismzb_86@hotmail.com>
2022-04-20 17:26:09 +0200

    project done

---
+++ README.md
@@ -0,0 +1,20 @@
+Objetivos:
+Aprender los conceptos básicos de programación y conseguir hacer click para aplicarlos (variables, estructuras de datos, condicionales, funciones, etc.). Realizar peticiones al servidor y almacenar datos en localstorage.
+
+Diseño y Creatividad:
+
+Planificación del proyecto
+Uso de HTML y maquetación con CSS.
+Uso del Adalab Web Started Kit (partials, anidación…).
+Gestión de eventos en el navegador: al hacer click, recargar, búsqueda errónea.
+Acceso y envío de datos a un servidor.
+Almacenamiento en local usando LocalStorage.
+
+Construido con:
+Lenguajes:
+HTML SCSS JAVASCRIPT
+
+Herramientas:
+VSC GITHUB GULP ADALAB WEB STARTER KIT NPM
+Autores:
+Iris M. Zuñel Ballesteros
---
+++ src/js/main.js
@@ -0,0 +1,79 @@
+"use strict";
+
+let users = [];
+
+const listUsers = document.querySelector(".js-users");
+const saveDatabtn = document.querySelector(".js-saveData");
+const loadDatabtn = document.querySelector(".js-loadData");
+
+function getData() {
+  fetch("https://randomuser.me/api/?results=10")
+    .then((response) => response.json())
+    .then(function (data) {
+      saveUsers(data);
+      paintUsers(users);
+      showBtn();
+      handleBtns();
+    })
+    .catch(showError);
+  function showError() {
+    alert("Datos no disponibles");
+    listUsers.innerHTML = `<button class="reload" onclick=location.reload()>Recargar</button>`;
+  }
+  function handleBtns() {
+    saveDatabtn.addEventListener("click", saveFriends);
+    loadDatabtn.addEventListener("click", loadFriends);
+
+    function saveFriends() {
+      localStorage.setItem("people", JSON.stringify(users));
+    }
+
+    function loadFriends() {
+      listUsers.innerHTML = "";
+      let saveUsers = JSON.parse(localStorage.getItem("people"));
+      paintUsers(saveUsers);
+    }
+  }
+
+  function saveUsers(data) {
+    const datos = data.results;
+    for (const dato of datos) {
+      let user = {
+        name: dato.name.first,
+        last: dato.name.last,
+        city: dato.location.city,
+        picture: dato.picture.medium,
+        username: dato.login.username,
+        isFriend: false,
+      };
+      users.push(user);
+    }
+  }
+}
+
+function showBtn() {
+  saveDatabtn.style.display = "initial";
+  loadDatabtn.style.display = "initial";
+}
+
+function paintUsers(users) {
+  for (let cont = 0; cont < users.length; cont++) {
+    const isFriend = users[cont].isFriend;
+    listUsers.innerHTML += `<div class="user" style=background-color:${isFriend ? "pink" : "lightblue"
+      } id=user${cont} onclick=makeFriend(${cont})><img class="photos" src=${users[cont].picture
+      }><br><b>${users[cont].name} ${users[cont].last}</b><br>${users[cont].city
+      }<br>${users[cont].username}<br></div>`;
+  }
+}
+
+function makeFriend(requestedUser) {
+  //selección no amigos/amigos
+  if (users[requestedUser].isFriend === false) {
+    users[requestedUser].isFriend = true;
+    //cambio de fondo
+    let userFriend = document.querySelector("#user" + requestedUser);
+    userFriend.style.background = "pink";
+  } else {
+    alert("ya es tu amigo");
+  }
+}
